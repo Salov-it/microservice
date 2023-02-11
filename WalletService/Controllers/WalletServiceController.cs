26bc93bbac1132e35249bb02b41e99c0d4bc1cc5
Salov-it <salovit96@gmail.com>
2023-02-11 16:56:35 +0300

    сделал создание кошелька

---
+++ WalletService/Controllers/WalletServiceController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WalletService;
+using static WalletService.WalletService;
+
+namespace WalletService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalletServiceController : ControllerBase
+    {
+  
+        // POST api/<WalletServiceController>
+        [HttpPost("Walletnew")]
+        public void Post(int ownerId, decimal balance, string createdAt, string updatedAt)
+        {
+          WalletService walletService = new WalletService();
+          walletService.Walletnew(ownerId, balance, createdAt, updatedAt);
+        }
+
+        
+    }
+}
---
+++ WalletService/WalletService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authentication.OAuth.Claims;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace WalletService
+{
+    public class WalletService
+    {
+        public class Wallet
+        {
+            //данные кошелька
+          [Key] public int Id { get; set; }
+          public int ownerId { get; set; }
+          public decimal balance { get; set; } 
+          public string? createdAt { get; set; }
+          public string? updatedAt { get; set; }
+
+        }
+
+        public class ApplicationContext: DbContext
+        {
+            public DbSet<Wallet> Wallets => Set<Wallet>();
+
+            public ApplicationContext() => Database.EnsureCreated(); //Определяет есть ли бд если нет создает новую
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlite("Data Source=WalletBasse.db");
+                
+            }
+        }
+
+        public void Walletnew(int ownerId, decimal balance, string createdAt, string updatedAt)
+        {
+            
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Wallet wallet = new Wallet { ownerId = ownerId,balance = balance, createdAt = createdAt ,updatedAt = updatedAt};
+                db.Wallets.Add(wallet);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+            }
+
+        }
+    }
+}
